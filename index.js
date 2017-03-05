4aa0fb027830f4b477cebecd819a48aeb4190021
Bubba Tings <admin@bulbthings.com>
2017-03-05 20:30:45 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const processArguments = require("./lib/processArguments.js")
+const getFromCache = require("./lib/getFromCache.js")
+const getFromFiles = require("./lib/getFromFiles.js")
+const crawlData = require("./lib/crawlData.js")
+const searchTags = require("./lib/searchTags.js")
+const respondAndExit = require("./lib/respondAndExit.js")
+
+let results = ""
+
+// First fetch the tags to be searched
+processArguments((err, tags) => {
+  if (err !== null) {
+    console.error("XXX - Usage : pipe, arg or tags.txt file")
+    process.exit(1)
+  }
+
+  console.log("====> " + tags.map(tag => "'" + tag + "'"))
+
+  // First try to get the results from cache
+  getFromCache((err, crawledData) => {
+    if (err === null) {
+      // If the cache was fresh, then we just need to
+      // reduce the results to the tags we're actually searching
+      results = searchTags(tags, crawledData)
+
+      respondAndExit(results)
+    } else {
+      // If cache is absent or stale, then fetch the data from the JSON files
+      getFromFiles((err, data) => {
+        if (err !== null) {
+          console.error("XXX - No data could be found. Exiting process...")
+          process.exit(1)
+        }
+
+        // Then crawl the JSON data to count tag occurrences
+        crawlData(data, crawledData => {
+          // And finally reduce the crawled data
+          // to the tags we're actually searching
+          results = searchTags(tags, crawledData)
+
+          respondAndExit(results)
+        })
+      })
+    }
+  })
+
+
+})
