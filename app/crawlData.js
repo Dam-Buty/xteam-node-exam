c3e4601d9b0ca2d450a9909a7b8ed41e87cfe39f
Bubba Tings <admin@bulbthings.com>
2017-03-15 10:10:46 +0000

    Turned findTagsLists into a pure function

--- app/crawlData.js
+++ app/crawlData.js
@@ -1,6 +1,34 @@
 'use strict'
 
 /**
+ * Recursively traverses all nested objects in the data
+ * and feeds tagsList with tags arrays
+ * (liberally inspired from http://stackoverflow.com/a/8085118)
+ * @param {object[]} obj
+ */
+const findTagsLists = obj => {
+  let tagsList = []
+
+  // traverse all properties
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      if (prop === "tags") {
+        // tags found! add them to the list
+        tagsList = tagsList.concat(obj[prop])
+      } else {
+        // recurse through all other object type properties
+        // (arrays are typeof 'object' too for some reason)
+        if (typeof obj[prop] === "object") {
+          tagsList = tagsList.concat(findTagsLists(obj[prop]))
+        }
+      }
+    }
+  }
+
+  return tagsList
+}
+
+/**
  * Crawls the data from the JSON files
  * And returns a crawled data object looking like this :
  * {
@@ -12,33 +40,7 @@
  * @return {object} crawledData
  */
   const crawlData = data => {
-    let tagsList = []
-
-    /**
-     * Recursively traverses all nested objects in the data
-     * and feeds tagsList with tags arrays
-     * (liberally inspired from http://stackoverflow.com/a/8085118)
-     * @param {object[]} obj
-     */
-    const findTagsLists = obj => {
-      // traverse all properties
-      for (let prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
-          if (prop === "tags") {
-            // tags found! add them to the list
-            tagsList = tagsList.concat(obj[prop])
-          } else {
-            // recurse through all other object type properties
-            // (arrays are typeof 'object' too for some reason)
-            if (typeof obj[prop] === "object") {
-              findTagsLists(obj[prop])
-            }
-          }
-        }
-      }
-    }
-
-    findTagsLists(data)
+    const tagsList = findTagsLists(data)
 
     // Build the crawledData object
     // filter(Boolean) just filters out falsy entries (null, undefined, etc...)
